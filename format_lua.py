16889bd31f0fd6ffbeac8e4e60702496196c0526
Wok <woctezuma@users.noreply.github.com>
2024-04-29 01:33:30 +0200

    Add an argument parser for CLI

--- format_lua.py
+++ format_lua.py
@@ -1,14 +1,21 @@
+import argparse
+
 from src.conversions import to_data, to_text
 from src.disk import load_text, save_text
 from src.utils import order_dict
 
 
-def main() -> None:
-    apply_sort = True
+def main(args: argparse.Namespace) -> None:
+    apply_sort = args.apply_sort
+    language = args.language
+    input_fname = args.input_fname
+    output_fname = args.output_fname
 
-    language = "fr"
-    input_fname = f"localization/{language}.lua"
-    output_fname = input_fname
+    if language:
+        input_fname = f"localization/{language}.lua"
+
+    if not output_fname:
+        output_fname = input_fname
 
     data = to_data(load_text(input_fname))
     new_data = order_dict(data) if apply_sort else data
@@ -16,4 +23,38 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--sort",
+        action="store_true",
+        dest="apply_sort",
+        help="Whether to sort the .lua file.",
+    )
+    parser.add_argument(
+        "-l",
+        "--language",
+        type=str,
+        default="",
+        dest="language",
+        help="Set the language to automatically select the input filename.",
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        default="localization/fr.lua",
+        dest="input_fname",
+        help="Input filename.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="",
+        dest="output_fname",
+        help="Output filename.",
+    )
+    args = parser.parse_args()
+
+    main(args)
